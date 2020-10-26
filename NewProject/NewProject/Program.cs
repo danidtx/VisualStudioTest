110e3c7964f2d5db6a029dc972ab515fb6264e9f
Daniel <danidtx@gmail.com>
2020-10-26 22:53:26 +0100

    NewProject created

--- NewProject/NewProject/Program.cs
+++ NewProject/NewProject/Program.cs
@@ -6,7 +6,7 @@ namespace NewProject
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello  World!");
         }
     }
 }
